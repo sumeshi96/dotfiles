4d3a6a6a88178cc2aa5a16643567c6e6f33cdab4
sumeshi96 <yumapc96@gmail.com>
2026-01-16 16:49:49 +0900

    Add .config/alacritty/alacritty.toml Add .config/alacritty/dracula.toml Add .config/sheldon/plugins.toml Add .config/starship.toml Add .config/zellij/config.kdl Add .config/zellij/themes/themes/dracula.kdl Add .gitconfig Add .vimrc Add .zshrc Add install_packages.sh

---
+++ run_once_install_packages.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#
+# package install 
+#
+
+# apt install
+sudo apt update
+sudo apt install zsh alacritty git build-essential libssl-dev pkg-config
+
+# change shell
+chsh -s $(which zsh)
+
+# starship install
+curl -sS https://starship.rs/install.sh | sh
+
+# rust install
+curl --proto '=httpshttps' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# cargo install
+cargo install zellij sheldon eza bat fd ripgrep delta broot zenith silicon
+
+#
+# font install
+#
+
+# 一時的な作業ディレクトリを作成して移動
+WORK_DIR="/tmp/hackgen_install"
+mkdir -p "$WORK_DIR"
+cd "$WORK_DIR"
+
+# HackGen v2.10.0 (Nerd Font版) のZIPをダウンロード
+wget https://github.com/yuru7/HackGen/releases/download/v2.10.0/HackGen_NF_v2.10.0.zip
+
+# ZIPファイルを展開
+unzip HackGen_NF_v2.10.0.zip
+
+# ユーザー個別のフォントディレクトリを作成
+FONT_DIR="$HOME/.local/share/fonts/HackGenNF"
+mkdir -p "$FONT_DIR"
+
+# .ttf ファイル（フォント本体）を展開先からフォントディレクトリに移動
+mv HackGen_NF_v2.10.0/*.ttf "$FONT_DIR/"
+
+echo "フォントファイルを $FONT_DIR にインストールしました。"
+
+# フォントキャッシュの削除
+fc-cache -fv
